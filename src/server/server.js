a0e69ccdfee34e7ef37713c1cba8c43d79c0b8ea
MHilmiAdz <103296966+MHilmiAdz@users.noreply.github.com>
2024-06-02 21:48:58 +0700

    Template API

---
+++ src/server/server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const routes = require('../server/routes');
+const loadModel = require('../services/loadModel');
+const InputError = require('../exceptions/inputError');
+
+(async () => {
+    const server = Hapi.server({
+        port: 8000,
+        host: '0.0.0.0',
+        routes: {
+            cors: {
+              origin: ['*'],
+            },
+        },
+    });
+
+    const model = await loadModel();
+    server.app.model = model;
+
+    server.route(routes);
+
+    //ON-PRERESPONSE FUNCTION, FOR HANDLING THE ERROR BEFORE IT BEGINS.
+    //THIS MACHINE WILL RUN THIS ALLL FIRST.
+    server.ext('onPreResponse', function (request, h) {
+        const response = request.response;
+
+        if (response instanceof InputError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: `${response.message} Silakan gunakan foto lain.`
+            })
+            newResponse.code(response.statusCode)
+            return newResponse;
+        }
+
+        if (response.isBoom) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: response.message
+            })
+            newResponse.code(response.output.statusCode)
+            return newResponse;
+        }
+
+        return h.continue;
+    });
+
+    await server.start();
+    console.log(`Server start at: ${server.info.uri}`);
+})();
